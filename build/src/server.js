3f14b72eddc9edd5224306eb1dd28df33f549e37
abdizamedmo <abdizamedmo@gmail.com>
2022-12-19 09:03:34 +0300

    Build ignored

--- build/src/server.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const apollo_1 = require('./graphql/apollo');
-const PORT = Number(process.env.PORT);
-(0, apollo_1.startApolloServer)(PORT);
